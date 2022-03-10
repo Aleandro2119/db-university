12d1f411c38fb51636a5a395e7d7bcd6bc97c907
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-10 17:17:44 +0100

    es 2

--- esercizio_sql.sql
+++ esercizio_sql.sql
@@ -3,3 +3,10 @@
 SELECT COUNT(*) AS 'iscritti_per_anno',  YEAR(`enrolment_date`) AS `year`
 FROM `students` 
 GROUP BY `year`;
+
+-- ? 2) Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) AS 'persone_ufficio', `office_address`
+FROM `teachers` 
+GROUP BY `office_address`;
+
